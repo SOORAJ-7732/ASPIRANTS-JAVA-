9c09bbfd4f3dd9c7794db28309ef6c41c4cc39f8
SOORAJ-7732 <soorajgsd620@gmail.com>
2026-02-23 22:44:33 +0530

    Add files via upload

---
+++ grt3.java
@@ -0,0 +1,18 @@
+class grt3{
+
+    public static void main(String args[])
+    {
+        int a = 10;int b = 20;int c = 30;
+
+        if(a>b && a>c){
+            System.out.println("A is greater");
+        }else if(b>a && b>c){
+            System.out.println("B is greater");
+        }else if(c>a && c>b){
+            System.out.println("c is greater");
+        }
+        else{
+            System.out.println("A,B and C are equal");
+        }
+    }
+}
